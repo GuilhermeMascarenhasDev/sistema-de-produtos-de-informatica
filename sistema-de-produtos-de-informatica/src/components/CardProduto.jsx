4339aac885e4759b6a1f0bafc0286fb901d9b0f7
Jheimisson <jheimissonsantos100@gmail.com>
2026-08-11 20:39:47 -0300

    adicionando o layout dos produtos

---
+++ sistema-de-produtos-de-informatica/src/components/CardProduto.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+export default function CardProduto({ produto }) {
+  return (
+    <div className="card-produto">
+
+      <img
+        src={produto.imagem}
+        alt={produto.modelo}
+      />
+
+      <div className="card-conteudo">
+        <h2>{produto.marca}</h2>
+
+        <h3>{produto.modelo}</h3>
+
+        <p>{produto.resumo}</p>
+
+        <Link
+          to={/produto/${produto.id}}
+          className="btn-detalhes"
+        >
+          Ver detalhes
+        </Link>
+      </div>
+
+    </div>
+  );
+}
