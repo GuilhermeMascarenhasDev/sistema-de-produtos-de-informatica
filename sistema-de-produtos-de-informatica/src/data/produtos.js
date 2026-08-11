9ce361791c0f0a8c671246d37fac3aafb716333b
senai15700-ux <senai15700@gmail.com>
2026-08-11 19:08:02 -0300

    Adiciona coleção de produtos de informática

---
+++ sistema-de-produtos-de-informatica/src/data/produtos.js
@@ -0,0 +1,49 @@
+export const produtos = [
+  {
+    id: 1,
+    marca: "Dell",
+    modelo: "Inspiron 15",
+    especificacoes:
+      "Notebook com processador Intel Core i5, 8 GB de memória RAM, SSD de 512 GB, tela de 15,6 polegadas Full HD e sistema operacional Windows 11.",
+    resumo:
+      "Notebook indicado para estudos, trabalho e tarefas do dia a dia.",
+    imagem:
+      "https://images.unsplash.com/photo-1496181133206-80ce9b88a853"
+  },
+
+  {
+    id: 2,
+    marca: "Lenovo",
+    modelo: "IdeaPad 3",
+    especificacoes:
+      "Notebook equipado com processador AMD Ryzen 5, 8 GB de RAM, SSD de 256 GB, tela de 15,6 polegadas e sistema operacional Windows 11.",
+    resumo:
+      "Computador portátil com bom desempenho para estudos e produtividade.",
+    imagem:
+      "https://images.unsplash.com/photo-1588872657578-7efd1f1555ed"
+  },
+
+  {
+    id: 3,
+    marca: "Logitech",
+    modelo: "G203",
+    especificacoes:
+      "Mouse gamer com sensor óptico de alta precisão, seis botões programáveis, iluminação RGB e conexão USB.",
+    resumo:
+      "Mouse desenvolvido para jogos e atividades que exigem precisão.",
+    imagem:
+      "https://images.unsplash.com/photo-1527814050087-3793815479db"
+  },
+
+  {
+    id: 4,
+    marca: "Kingston",
+    modelo: "NV2 1TB",
+    especificacoes:
+      "SSD NVMe com capacidade de 1 TB, interface PCIe 4.0, alta velocidade de leitura e gravação e formato M.2 2280.",
+    resumo:
+      "Armazenamento rápido para computadores e notebooks compatíveis.",
+    imagem:
+      "https://images.unsplash.com/photo-1597872200969-2b65d56bd16b"
+  }
+];
